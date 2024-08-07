c1ff1b09b3e5c8ad8c67a411c15ab6102addf91e
Erisson10 <157748471+Erisson10@users.noreply.github.com>
2024-08-07 15:03:44 -0300

    comitei

--- scripts.js
+++ scripts.js
@@ -1,22 +1,23 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
+const buttonMapAll
+
 let myLi = ''
 
 function showAll() {
-    menuOptions.forEach((product) => {
-        myLi += `
-      <li>
-            <img src=${product.src}>
-            <p>${product.name}</p>
-            <p class="item-price">R$ ${product.price}</p>
-      </li>
-      `
-    })
+  menuOptions.forEach((product) => {
+    myLi += `
+  <li>
+      <img src=${product.src}>
+      <p>${product.name}</p>
+      <p class="item-price">R$ ${product.price}</p>
+  </li>
+  `
+  })
 
-    list.innerHTML = myLi
+  list.innerHTML = myLi
 }
 
 
 
 buttonShowAll.addEventListener('click', showAll)
-
